9faa10d2943ac1229d02ee0b01e0c784326a0144
Iaes ~ Laptop <Ian.stevens@STUDENT.PXL.BE>
2024-02-19 12:27:07 +0100

    Project construction

---
+++ DependencyInjectionDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInjectionDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DependencyInjectionDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
